81fb2b93f37eae62d0d2140c78378003b7d86b20
Darius Stewart <darius.stewart31@gmail.com>
2019-07-29 06:26:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,13 +4,11 @@
 def fizzbuzz(num)
   if num % 3 == 0
     puts "Fizz"
-  end
 elsif num % 5 == 0
   puts "Buzz"
-end
 elsif num % 3 == 0 && num % 5 == 0
   puts "FizzBuzz"
-end
 else
   nil
+end
 end
