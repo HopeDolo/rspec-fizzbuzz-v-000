753549b0118bb8ac213d84275c66076221787948
Darius Stewart <darius.stewart31@gmail.com>
2019-07-29 06:16:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  if num % 3 == 0
+    puts "Fizz"
+  end
+elsif num % 5 == 0
+  puts "Buzz"
+end
+elsif num % 3 == 0 && num % 5 == 0
+  puts "FizzBuzz"
+end
+else
+  nil
+end
